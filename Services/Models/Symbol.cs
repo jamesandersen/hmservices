1e21fb9347449b67f866e2c63754c21782ed08f9
James Andersen <james.andersen@universalmind.com>
2016-11-19 14:46:14 -0700

    Fix CORS config and add additional Symbol properties

--- Services/Models/Symbol.cs
+++ Services/Models/Symbol.cs
@@ -1,5 +1,6 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
 
 namespace HMServices.Models {
         [BsonIgnoreExtraElements]
@@ -7,8 +8,16 @@ namespace HMServices.Models {
         public ObjectId Id { get; set; }
 
         [BsonElement("Symbol")]
+        [JsonProperty("Symbol")]
         public string Ticker { get; set; }
         public string Name { get; set; }
+
+        public string Sector { get; set; }
+        public string Industry {get; set; }
+
+        public decimal MarketCap { get; set; }
+
+        public string exchange { get; set; }
     }
 
 
